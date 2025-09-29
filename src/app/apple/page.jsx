c1ebf17f28427d90275a42a06678dac06e211bcf
boniface193 <bonifacedennis99@gmail.com>
2025-09-29 19:10:12 +0100

    updated coming soon

--- src/app/apple/page.jsx
+++ src/app/apple/page.jsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React, { useState } from 'react'
 import { FiMenu, FiSearch, FiShoppingBag } from 'react-icons/fi'
 import { FaFacebookF, FaTwitter, FaLinkedinIn } from 'react-icons/fa'
