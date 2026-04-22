64f25d031e4ec32307926fb8a05074ff6e1ff8cc
deboik193 <deboik193@gmail.com>
2026-04-22 17:25:41 +0100

    fix: added serviceType to user service

--- services/userService.js
+++ services/userService.js
@@ -421,7 +421,7 @@ class UserService {
   /**
    * Find nearby users (for runners to find customers)
    */
-  async findNearbyUsers({ latitude, longitude, fleetType }) {
+  async findNearbyUsers({ latitude, longitude, serviceType, fleetType }) {
     try {
       return await User.findNearbyUsers({
         latitude,
