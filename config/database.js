e7db5ae0babbffa8afdf0bc8edcc31f6b43b8e55
deboik193 <deboik193@gmail.com>
2026-06-27 08:17:47 +0100

    debug in prod

--- config/database.js
+++ config/database.js
@@ -25,7 +25,7 @@ const connectDb = async () => {
 
     // await User.deleteMany({ role: { $nin: ['super-admin', 'admin'] } });
     // await Runner.deleteMany({});
-    // await Runner.findOneAndUpdate({ email:'timivictor565@gmail.com' }, { $set: { kycStatus: 'approved_full' } });
+    // await Runner.findOneAndUpdate({ email:'tinukareem17@gmail.com' }, { $set: { fleetType: 'cycling' } });
     // await Runner.deleteOne({ email: 'timivictor565@gmail.com' });
 
   } catch (error) {
