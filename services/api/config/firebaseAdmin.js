0f412ced20b679b195586960ce822ecaa49a0d86
deboik193 <deboik193@gmail.com>
2026-07-06 17:18:57 +0100

    fix: replaced invalid firebase credential

--- services/api/config/firebaseAdmin.js
+++ services/api/config/firebaseAdmin.js
@@ -2,7 +2,7 @@ const admin = require('firebase-admin');
 const path = require('path');
 
 // Path
-const serviceAccount = require(path.join(__dirname, 'sendrey-6f52d-firebase-adminsdk-fbsvc-c45466bf0a.json'));
+const serviceAccount = require(path.join(__dirname, 'serviceAccountCredential.json'));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
