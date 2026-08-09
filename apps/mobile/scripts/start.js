63512f105d2d012097f86d0bf16b1c68d85fca71
aarenchyma <timivictor565@gmail.com>
2026-08-09 21:32:11 +0100

    fix: added React CRA prefix to mobile script

--- apps/mobile/scripts/start.js
+++ apps/mobile/scripts/start.js
@@ -2,7 +2,7 @@ const net = require('net');
 const path = require('path');
 const { spawn } = require('child_process');
 
-const startPort = Number(process.env.MOBILE_PORT || 3001);
+const startPort = Number(process.env.REACT_APP_MOBILE_PORT || 3001);
 const maxAttempts = 20;
 
 function getAvailablePort(port, attempt = 1) {
