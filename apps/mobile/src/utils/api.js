a794212772855f4603c92e1b7370ae9e782ded40
aarenchyma <timivictor565@gmail.com>
2026-08-09 21:30:16 +0100

    fix: added CRA prefix in start.js and fixed dep issue in userDispute

--- apps/mobile/src/utils/api.js
+++ apps/mobile/src/utils/api.js
@@ -5,6 +5,7 @@ import { authStorage } from "./authStorage";
 export const isCapacitor = !!(window.Capacitor && window.Capacitor.isNativePlatform && window.Capacitor.isNativePlatform());
 
 const BASE_URL = process.env.REACT_APP_API_URL;
+console.log(BASE_URL);
 
 const api = axios.create({
   baseURL: BASE_URL,
